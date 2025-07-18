ec6d0169e1fa185233414081bb2393d01862a203
claudsaints <claudiodsantoss7@gmail.com>
2025-07-17 21:08:56 -0300

    app-initialized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { SignInComponent } from './pages/signin/sign-in.component';
+import { SignUpComponent } from './pages/signup/sign-up.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "" , component: LandingComponent},
+    {path: "signin" , component: SignInComponent},
+    {path: "signup" , component: SignUpComponent},
+    {path: "home" , component: HomeComponent},
+    
+];
